f1aea102127c9b725817a1deed0fd7171267e4ac
得邪 <2281741446@qq.com>
2022-11-01 21:49:24 +0800

    配置文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -24,45 +21,3 @@ function App() {
 }
 
 export default App;
-
-
-const elementObj = {
-  PROCESSED: [
-    { color: { true: "red", false: "" }, content: "Uncheck" },
-    { color: { true: "red", false: "" }, content: "Wait Archives Group Check" },
-    {
-      color: { true: "red", false: "" },
-      content: "Wait Risk Management Group Check",
-    },
-    { color: { true: "red", false: "" }, content: "Check" },
-  ],
-  DATA_UNMATCH: [
-    null,
-    null,
-    null,
-    null,
-    { color: { true: "red", false: "" }, content: "Wait Archives Group Check" },
-    { color: { true: "red", false: "" }, content: "Check" },
-  ],
-};
-const colorObj = {
-  PROCESSED: [
-    filesOperate || riskOperate,
-    filesOperate,
-    riskOperate,
-    operateOperate,
-  ],
-  DATA_UNMATCH: [false, false, false, false, filesOperate, brokerOperate],
-};
-const colorFlag1 = colorObj[recorder.state] || [];
-const colorFlag = colorFlag1[val] || false;
-
-const elementFlag = elementObj[recorder.state] || [
-  { color: { true: "", false: "" }, content: "" },
-];
-const { color, content } = elementFlag[val] || {
-  color: { true: "", false: "" },
-  content: "-",
-};
-
-return <span style={{ color: color[colorFlag] }}>{content}</span>;
